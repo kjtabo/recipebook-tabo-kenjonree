1a958e12501b967960c4822a8f267f66f04e6b9b
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-02-27 18:51:53 +0800

    added features - models for Recipe, Ingredient, RecipeIngredient - added CBV for recipe list and recipe detail - modified templates to work with CBV - modified views.py to make use of CBV - implemented admin panel

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+    fieldsets = (
+        ("Recipe and Ingredient Information", {
+            "fields": (
+                "quantity", ("ingredient", "recipe")
+            ),
+        }),
+    )
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline,]
+    fieldsets = (
+        ("Recipe Information", {
+            "fields": (
+                "name",
+            ),
+        }),
+    )
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    fieldsets = (
+        ("Ingredient Information", {
+            "fields": (
+                "name",
+            ),
+        }),
+    )
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
