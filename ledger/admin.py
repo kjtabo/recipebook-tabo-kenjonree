02b59c65c74ed057ed864b3c365fa0c73e04067e
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-03-10 23:15:07 +0800

    moved Profile model to a different app

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,17 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient, Profile
-
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-
-
-class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline,]
+from .models import Recipe, Ingredient, RecipeIngredient
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -43,9 +31,6 @@ class IngredientAdmin(admin.ModelAdmin):
         }),
     )
 
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-    )
-    name = models.CharField(max_length=50)
-    bio = models.TextField(blank=True)
+from useraccounts.models import Profile
 
 
 class Ingredient(models.Model):
