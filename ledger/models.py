8349657a573d4d8adf5fe0fd72bf918261e1650c
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-03-10 21:28:12 +0800

    added back password reset

--- ledger/models.py
+++ ledger/models.py
@@ -11,9 +11,6 @@ class Profile(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField(blank=True)
 
-    def __str__(self):
-        return self.name
-
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
