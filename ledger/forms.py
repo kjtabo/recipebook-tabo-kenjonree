f81be5ed4b5490685893caabf25e48e53135e833
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-03-24 21:26:21 +0800

    cleaned code - fixed hardcoded urls in templates - organized imports

--- ledger/forms.py
+++ ledger/forms.py
@@ -6,12 +6,12 @@ from .models import Recipe, RecipeImage
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ["name"]
-        # fields = "__all__"
+        # fields = ["name"]
+        fields = "__all__"
 
 
 class RecipeImageForm(forms.ModelForm):
     class Meta:
         model = RecipeImage
-        # fields = ["image", "description"]
-        fields = "__all__"
+        fields = ["image", "description"]
+        # fields = "__all__"
--- ledger/views.py
+++ ledger/views.py
@@ -1,8 +1,11 @@
 from django.urls import reverse_lazy
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Recipe, RecipeImage
 from .forms import RecipeForm, RecipeImageForm
 
