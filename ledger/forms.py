7d1d5bee207bfd558795091706d951305b32902b
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-03-24 23:55:58 +0800

    finalized

--- ledger/forms.py
+++ ledger/forms.py
@@ -6,7 +6,6 @@ from .models import Recipe, RecipeImage
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        # fields = ["name"]
         fields = "__all__"
 
 
@@ -14,4 +13,3 @@ class RecipeImageForm(forms.ModelForm):
     class Meta:
         model = RecipeImage
         fields = ["image", "description"]
-        # fields = "__all__"
